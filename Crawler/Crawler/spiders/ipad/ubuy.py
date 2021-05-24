8c96cb1693e579ddea86205e953a70e842e05b50
NguyenQuyPhuc20173302 <nguyenquyphuc1591999@gmail.com>
2021-05-24 17:38:05 +0700

    add watch viettelstore

--- Crawler/Crawler/spiders/ipad/ubuy.py
+++ Crawler/Crawler/spiders/ipad/ubuy.py
@@ -8,14 +8,14 @@ from Crawler.matching import *
 
 class uBuy(scrapy.Spider):
     name = 'ipad_ubuy'
-    start_urls = ["https://www.jbhifi.com.au/collections/computers-tablets/apple-ipads"]
+    start_urls = ["https://imagineonline.store/t/category/watch"]
 
     script = """
             function main(splash)
                 local url = splash.args.url
                 assert(splash:go(url))
                 assert(splash:wait(2))
-                assert(splash:runjs('try{document.getElementsByClassName("ais-Pagination-link")[0].click();}catch(e){}'))
+                assert(splash:runjs('document.getElementsByClassName("next_page page-item")[0].click();'))
                 assert(splash:wait(5))
                 return {
                     html = splash:html(),
@@ -23,18 +23,6 @@ class uBuy(scrapy.Spider):
                 }
             end
             """
-    script1 = """
-                function main(splash)
-                    local url = splash.args.url
-                    assert(splash:go(url))
-                    assert(splash:wait(2))
-                    assert(splash:wait(5))
-                    return {
-                        html = splash:html(),
-                        url = splash:url(),
-                    }
-                end
-                """
 
     def start_requests(self):
         headers = {
@@ -44,16 +32,19 @@ class uBuy(scrapy.Spider):
                 url,
                 callback=self.parse,
                 headers=headers,
-                meta={
-                    "splash": {"endpoint": "execute", "args": {"lua_source": self.script1}}
-                },
             )
 
     def parse(self, response):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        items = response.xpath('//*[@id="collection-search-results"]/div/div').css('div.ais-hits--item')
-        print(items)
+        items = response.xpath('//*[@id="content"]/div/div[4]/div[1]/div/div/div/div[1]').css('div.overflow-hidden')
+        for item in items:
+            thongtin = dict()
+            thongtin['name'] = str(item.css('h5::text').get())
+            thongtin['price'] = str(item.css('div.flex-grow-1 span::text').get())
+
+            yield convert(thongtin)
+
         yield SplashRequest(
             response.url,
             callback=self.parse,
